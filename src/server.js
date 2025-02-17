dc85033ccee7347db6691e2c6a793c7719c917ed
ahtisham02 <ahtisham02>
2025-02-17 19:23:43 +0500

    chnges

--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
